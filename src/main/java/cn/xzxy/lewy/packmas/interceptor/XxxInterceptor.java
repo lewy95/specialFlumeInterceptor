f5c252cc7a44a8900b61e107043007c6ee0fe62a
lewy95 <171133755@qq.com>
2020-01-28 10:54:02 +0800

    add zmy

---
+++ src/main/java/cn/xzxy/lewy/packmas/interceptor/XxxInterceptor.java
@@ -0,0 +1,31 @@
+package cn.xzxy.lewy.packmas.interceptor;
+
+import org.apache.flume.Event;
+import org.apache.flume.interceptor.Interceptor;
+
+import java.util.List;
+
+/**
+ * 待补充
+ */
+public class XxxInterceptor implements Interceptor {
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public Event intercept(Event event) {
+        return null;
+    }
+
+    @Override
+    public List<Event> intercept(List<Event> list) {
+        return null;
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
